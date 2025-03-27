9a886705eddb5e2c1629f26669ec1cc06cebb4c2
shajin <shajinshajin02@gmail.com>
2025-03-27 23:05:04 +0530

    nn

--- src/components/component/Education.js
+++ src/components/component/Education.js
@@ -1,39 +1,39 @@
 import React from 'react'
 import './css/Education.css'
+
 function Education() {
   return (
-    <div>
+    <div className="animate-fade-in">
+      <div className='edu-container'>
+        <div className='edu animate-slide-down'>
+          <h1>EDUCATION</h1>
+        </div>
+        
+        <div className='edu-content animate-slide-right hover-card'>
+          <div className='edu-clg-img hover-scale'> 
+            <img src='images/clg.png' alt="College"></img>
+          </div>
+          <h4 className="hover-highlight">Bachelor of Engineering in Computer Science</h4>
+          <li className="hover-translate">Bethlahem Institute of Engineering Karungal,Kanyakumari District</li>
+          <li className="hover-translate">CGPA : 79.0</li>
+          <li><a href='https://www.bethlahem.org/' className="hover-link">Bethlahem.org</a></li>
+        </div>
+        <div className='edu-date-1 animate-fade-in'>
+          <p>Nov 2020 - Jul 2024</p>
+        </div>
 
-<div className='edu-container'>
-<div className='edu'>
-  <h1>EDUCATION</h1>
-</div>
-<div className='edu-content'>
-  <div className='edu-clg-img'> 
-<img src='images/clg.png'></img>
-  </div>
-  <h4>Bachelor of Engineering in Computer Science</h4>
-  <li>Bethlahem Institute of Engineering Karungal,Kanyakumari District</li>
-  <li>CGPA : 79.0</li>
-  <li><a href='https://www.bethlahem.org/'>Bethlahem.org</a></li>
-</div>
-<div className='edu-date-1'>
-<p>Nov 2020 - Jul 2024</p>
-</div>
-
-<div className='edu-content-2'>
-<div className='edu-clg-img'> 
-<img src='images/school.png'></img>
-  </div>
-  <h4>Higher School Education</h4>
-  <li>Government Higher Secondary School Thuckalay</li>
-  <li>Percentage: 75%</li>
-</div>
-
-<div className='edu-date-1'>
-<p>Aug 2019 - Apr 2020</p>
-</div>
-</div>
+        <div className='edu-content-2 animate-slide-left hover-card'>
+          <div className='edu-clg-img hover-scale'> 
+            <img src='images/school.png' alt="School"></img>
+          </div>
+          <h4 className="hover-highlight">Higher School Education</h4>
+          <li className="hover-translate">Government Higher Secondary School Thuckalay</li>
+          <li className="hover-translate">Percentage: 75%</li>
+        </div>
+        <div className='edu-date-1 animate-fade-in'>
+          <p>Aug 2019 - Apr 2020</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/screens/HomePage.jsx
+++ src/components/screens/HomePage.jsx
@@ -11,31 +11,28 @@ function HomePage() {
 
   return (
     <div>
-      <header className="header">
-        <a href="#" className="logo">SHAJIN.S</a>
-        <Navbar onNavChange={handleNavChange} />
+      <header className="header animate-header">
+        <a href="#" className="logo animate-logo">SHAJIN.S</a>
+        <Navbar onNavChange={handleNavChange} activePage={data} />
       </header>
 
       {data === 'home' ? (
         <section className="home">
           <div className="home-content">
-            <h1 className="varela-round-regular">Hi, I'm SHAJIN.S</h1>
-            <h3>Java FullStack Developer</h3>
-            <div className='home-p'>
-            <p>
-               I completed my Bachelor's degree in Computer Science and Engineering in 2024.
-              During my studies, I built a strong foundation in <b>Core JAVA</b>, and after graduation, I completed an <b>internship in Java Full Stack</b> at Srishti Innovative.
-              During this internship, I gained hands-on experience in <b>Core Java,</b> <b>Spring Boot,</b> <b>MySQL,</b> and <b>React JS,</b> and 
-              became familiar with developing <b>REST APIs</b> and database operations.
-              I also completed a project for a fully functional web application like an e-commerce platform for buying orchid plants, using <b>Spring Boot</b> for the backend and <b>React JS</b> for the frontend.
-            </p>
+            <h1 className="varela-round-regular animate-title">Hi, I'm SHAJIN.S</h1>
+            <h3 className="animate-subtitle">Java FullStack Developer</h3>
+            <div className='home-p animate-text'>
+            <p> I completed my Bachelor's degree in Computer Science and Engineering in 2024. During my studies, I built a strong foundation in <b>Core JAVA</b>, and after graduation, I completed an <b>internship in Java Full Stack</b> at <b>Algol</b>. During this internship, I gained hands-on experience in <b>Core Java,</b> <b>Spring Boot,</b> <b>MySQL,</b> and <b>React JS,</b> and became familiar with developing <b>REST APIs</b> and database operations. I also worked on projects involving microservices architecture, user management systems, and backend development best practices.
+Currently, I am working as a <b>Java Developer</b> at <b>SASOFT Trivandrum</b>, a company specializing in delivering custom software solutions and enterprise applications. Here, I am involved in developing scalable and efficient backend systems using <b>Spring Boot</b> and microservices architecture.
+
+</p>
             </div>
-            <div className="btn-box btn-hire-talk">
+            <div className="btn-box btn-hire-talk animate-buttons">
               <a href="mailto:shajinshajin02@gmail.com">Hire me</a>
               <a href="mailto:shajinshajin02@gmail.com">Talk</a>
             </div>
           </div>
-          <div className="home-sci">
+          <div className="home-sci animate-social">
             <a href="https://www.linkedin.com/in/shajin-s-52394629a"><i className="bx bxl-linkedin-square"></i></a>
             <a href="https://github.com/shajinshan"><i className="bx bxl-github"></i></a>
             <a href="https://www.instagram.com/shajinshan_/"><i className="bx bxl-instagram"></i></a>
